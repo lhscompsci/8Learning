dcc98cb23eb9a9cc1af72ac44075a1a73c3cb897
Black, Samuel Thomas <sam.black@gatech.edu>
2019-09-30 15:51:10 -0500

    playing with the Maths

--- learning.py
+++ learning.py
@@ -7,3 +7,12 @@ print("hello world.")
 print("howdy, y'all!")
 print("I like typing this.")
 print("Printing....Yay!!!")
+
+# Practice with Maths and things
+print("I will now count my chickens.")
+print("Hens", 25 + 30 / 6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now, I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
