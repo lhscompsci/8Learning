0e06bcb712565dca732e257704b4171e52c37fc5
Black, Samuel Thomas <sam.black@gatech.edu>
2019-09-30 15:33:55 -0500

    playing with octothorpes

--- learning.py
+++ learning.py
@@ -1,3 +1,8 @@
+#  What is an octothorpe?
+# <- This is an octothorpe.
+#  What is the octothorpe used for in python?
+# Notes, also known in the programming community as Comments
+
 print("hello world.")
 print("howdy, y'all!")
 print("I like typing this.")
